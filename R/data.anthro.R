e81856b0df01d79232150df7c26f65d8205cb0e1
Stuart Wheater <stuart.wheater@arjuna.com>
2021-01-03 10:21:29 +0000

    Changes to incorporate 'ANTHRO' dataset (#1)

---
+++ R/data.anthro.R
@@ -0,0 +1,57 @@
+#' Simulated dataset ANTHRO 1
+#'
+#' Simulated dataset ANTHRO 1, in a data.frame with 873 observations of 5 harmonized
+#' variables. The ANTHRO dataset contains synthetic data based. This dataset does not
+#' contain any NA values.
+#'
+#' | **Variable** | **Description**             | **Type** | **Note** |
+#' | ------------ | --------------------------- | -------- | -------- |
+#' | age          | Age of child                | numeric  | months   |
+#' | sex          | Gender of child             | integer  |          |
+#' | weight       | Weight of child             | numeric  | kg       |
+#' | height       | Height of child             | numeric  | cm       |
+#' | muac         | Mid-upper arm circumference | numeric  | cm       |
+#'
+#' @name anthro1
+#' @docType data
+#' @keywords data
+NULL
+
+#' Simulated dataset ANTHRO 2
+#'
+#' Simulated dataset ANTHRO 2, in a data.frame with 796 observations of 5 harmonized
+#' variables. The ANTHRO dataset contains synthetic data based. This dataset does not
+#' contain any NA values.
+
+#'
+#' | **Variable** | **Description**             | **Type** | **Note** |
+#' | ------------ | --------------------------- | -------- | -------- |
+#' | age          | Age of child                | numeric  | months   |
+#' | sex          | Gender of child             | integer  |          |
+#' | weight       | Weight of child             | numeric  | kg       |
+#' | height       | Height of child             | numeric  | cm       |
+#' | muac         | Mid-upper arm circumference | numeric  | cm       |
+#'
+#' @name anthro2
+#' @docType data
+#' @keywords data
+NULL
+
+#' Simulated dataset ANTHRO 3
+#'
+#' Simulated dataset ANTHRO 3, in a data.frame with 221 observations of 5 harmonized
+#' variables. The ANTHRO dataset contains synthetic data based. This dataset does not
+#' contain any NA values.
+#'
+#' | **Variable** | **Description**             | **Type** | **Note** |
+#' | ------------ | --------------------------- | -------- | -------- |
+#' | age          | Age of child                | numeric  | months   |
+#' | sex          | Gender of child             | integer  |          |
+#' | weight       | Weight of child             | numeric  | kg       |
+#' | height       | Height of child             | numeric  | cm       |
+#' | muac         | Mid-upper arm circumference | numeric  | cm       |
+#'
+#' @name anthro3
+#' @docType data
+#' @keywords data
+NULL
